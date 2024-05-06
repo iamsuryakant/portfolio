c2a8797fecce7dee5b9959f5d4053efcadc2f535
iamsuryakant <suryakantthakur825@gmail.com>
2024-05-06 15:23:25 +0530

    modified next conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    experimental: {
+        serverActions: true,
+    },
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
