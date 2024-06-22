6334910905d4f9e348c400944f6587e17e1786f1
Suryakant Thakur <suryakantthakur825@gmail.com>
2024-06-22 13:17:34 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2024 <span className="font-semibold">Suryakant Thakur</span>. All rights reserved.
+        &copy; {new Date().getFullYear()} <span className="font-semibold">Suryakant Thakur</span>. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold"> Made with lots of 🍵 + ❤️ + 🎵 in India.</span> 
