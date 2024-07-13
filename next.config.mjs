e2035733830cab265c25970e60921ad5f5c37c01
Suryakant Thakur <suryakantthakur825@gmail.com>
2024-07-13 21:57:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
+/* config options here */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable Image Optimization for static export
-  },
-  trailingSlash: true,
-  output: 'export',
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    distDir: 'out', // Where to export all pages
+    trailingSlash: true,
+    assetPrefix: '.',
+
+
+    // time in seconds of no pages generating during static
+    // generation before timing out
+    staticPageGenerationTimeout: 1000,
+    reactStrictMode: false
 };
 
 export default nextConfig;
